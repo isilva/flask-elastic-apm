86e4b6485de3f443af471c6cb4de4f5a7ea52cb9
Isaac Silva <isaac.silva@gmail.com>
2022-07-09 18:53:58 -0300

    apm integration

--- flaskr_app/__init__.py
+++ flaskr_app/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-
+from elasticapm.contrib.flask import ElasticAPM
 
 def create_app(test_config=None):
     # create and configure the app
@@ -10,7 +10,8 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr_app.sqlite'),
     )
-    
+    apm = ElasticAPM(app)
+
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
